1eb839658e0c0d250e05fa87f278a22db07ac241
rbianucci <rbianucci@uade.edu.ar>
2024-06-04 11:27:40 -0300

    feat: images added to API

---
+++ src/main/java/com/nopay/nopayapi/controller/ImagesController.java
@@ -0,0 +1,61 @@
+package com.nopay.nopayapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nopay.nopayapi.entity.Image;
+import com.nopay.nopayapi.service.ImageService;
+
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.Base64;
+import java.util.List;
+
+import javax.sql.rowset.serial.SerialException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("images")
+public class ImagesController {
+
+    @Autowired
+    private ImageService imageService;
+
+    @GetMapping
+    public ResponseEntity<List<Image>> getAllImages() {
+        List<Image> images = imageService.getAllImages();
+        return ResponseEntity.ok().body(images);
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> addImage(@RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file) {
+        try {
+            // Obtener los bytes de la imagen
+            byte[] bytes = file.getBytes();
+
+            // Crear una nueva instancia de Image con el nombre y los bytes de la imagen
+            Image image = new Image();
+            image.setImageBytes(bytes);
+
+            // Guardar la imagen en la base de datos
+            imageService.save(image);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body("Image added successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add image.");
+        }
+    }
+}
---
+++ src/main/java/com/nopay/nopayapi/entity/Image.java
@@ -0,0 +1,29 @@
+package com.nopay.nopayapi.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "image")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "image")
+    private byte[] imageBytes;
+
+    @Column(name = "date")
+    private Date date = new Date();
+
+}
